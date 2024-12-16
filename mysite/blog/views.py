ca6f85d49909860bc35778acffc9ce67ec639bcf
Galal Ewida <galalamrewida@gmail.com>
2024-12-16 16:41:43 +0200

    add pagenations

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # # Create your views here.
 # def home(request):
 #     return render(request,'home.html')
@@ -9,8 +9,16 @@ from django.http import Http404
 #     return render(request,'about.html')
 
 def post_list(request):
-    Posts = Post.objects.all()
-    return render(request,'blog/post/list.html',{'posts':Posts})
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list,per_page=2)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    return render(request,'blog/post/list.html',{'posts':posts})
 
 def post_detail(request,year,month,day,post):
     post = get_object_or_404(Post,status=Post.Status.PUBLISHED,
